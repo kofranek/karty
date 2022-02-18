aa716eafdb7ba0abb2872fc0efbe2d02e67b3f3b
Hollarka <kofranek@gmail.com>
2022-02-18 06:18:24 +0100

    obrázek

---
+++ index.js
@@ -0,0 +1,9 @@
+//img src="images/2_of_clubs.svg.png"
+
+let img = document.createElement("img")
+
+img.src = "images/2_of_clubs.svg.png"
+
+let domElement = document.getElementById('obr')
+
+domElement.appendChild(img)
