96ad90811bcfcc07a31b8704236050d3e34a682c
Hollarka <kofranek@gmail.com>
2022-02-18 06:48:15 +0100

    querySelector

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ let img = document.createElement("img")
 
 img.src = "images/2_of_clubs.svg.png"
 
-let domElement = document.getElementById('obr')
+let domElement = document.querySelector('#obr')
 
 domElement.appendChild(img)
