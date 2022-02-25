5c3d3a9939bb00c0589acf0ba168809d8c0a6d1a
kofranek <jiri.kofranek@lf1.cuni.cz>
2022-02-25 06:43:51 +0100

    přidání komentářů do index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-//img src="images/2_of_clubs.svg.png"
-
+//vytvoření elementu DOMu <img> (DOM - Document Object Model)
 let img = document.createElement("img")
 
+//přiřazení atributu src
 img.src = "images/2_of_clubs.svg.png"
 
+//nalezení identifikátoru obr
 let domElement = document.querySelector('#obr')
 
+//připojení emementu k DOMu
 domElement.appendChild(img)
